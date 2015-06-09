fcc24866e75453efe6262836be562058bdd133e2
Iamnvincible <ljlovemichael@hotmail.com>
2015-06-09 23:19:25 +0800

    改UI为白色

--- Date/MainPage.xaml.cs
+++ Date/MainPage.xaml.cs
@@ -42,13 +42,13 @@ namespace Date
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             // TODO: 准备此处显示的页面。
-
+            Windows.UI.ViewManagement.StatusBar.GetForCurrentView().HideAsync();
             // TODO: 如果您的应用程序包含多个页面，请确保
             // 通过注册以下事件来处理硬件“后退”按钮:
             // Windows.Phone.UI.Input.HardwareButtons.BackPressed 事件。
             // 如果使用由某些模板提供的 NavigationHelper，
             // 则系统会为您处理该事件。
-            
+
         }
     }
 }

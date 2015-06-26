7d2ea2e22a36f7a4f91dfb8f54a5b4bcc5728de2
yyx290799684 <yyx290799684@hotmail.com>
2015-06-26 21:59:13 +0800

    增加进度条

--- Date/SetHeadPage.xaml.cs
+++ Date/SetHeadPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Date.Util;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -8,6 +9,7 @@ using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics.Display;
@@ -15,6 +17,7 @@ using Windows.Graphics.Imaging;
 using Windows.Phone.UI.Input;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -76,56 +79,103 @@ namespace Date
 
         private async void AcceptAppBarButton_Click(object sender, RoutedEventArgs e)
         {
-            HttpClient _httpClient = new HttpClient();
-            CancellationTokenSource _cts = new CancellationTokenSource();
-
-
-            RenderTargetBitmap mapBitmap = new RenderTargetBitmap();
-            await mapBitmap.RenderAsync(headScrollViewer);
-            var pixelBuffer = await mapBitmap.GetPixelsAsync();
-            IStorageFolder applicationFolder = ApplicationData.Current.LocalFolder;
-            IStorageFile saveFile = await applicationFolder.CreateFileAsync("head.png", CreationCollisionOption.OpenIfExists);
-            using (var fileStream = await saveFile.OpenAsync(FileAccessMode.ReadWrite))
+            showStatus("保存中...", 1, Visibility.Visible);
+            string head = "";
+            try
             {
-                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
 
-                encoder.SetPixelData(
-                    BitmapPixelFormat.Bgra8,
-                    BitmapAlphaMode.Ignore,
-                    (uint)mapBitmap.PixelWidth,
-                    (uint)mapBitmap.PixelHeight,
-                    DisplayInformation.GetForCurrentView().LogicalDpi,
-                    DisplayInformation.GetForCurrentView().LogicalDpi,
-                    pixelBuffer.ToArray());
-                await encoder.FlushAsync();
+                HttpClient _httpClient = new HttpClient();
+                CancellationTokenSource _cts = new CancellationTokenSource();
+
+
+                RenderTargetBitmap mapBitmap = new RenderTargetBitmap();
+                await mapBitmap.RenderAsync(headScrollViewer);
+                var pixelBuffer = await mapBitmap.GetPixelsAsync();
+                IStorageFolder applicationFolder = ApplicationData.Current.LocalFolder;
+                IStorageFile saveFile = await applicationFolder.CreateFileAsync("head.png", CreationCollisionOption.OpenIfExists);
+                using (var fileStream = await saveFile.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
+
+                    encoder.SetPixelData(
+                        BitmapPixelFormat.Bgra8,
+                        BitmapAlphaMode.Ignore,
+                        (uint)mapBitmap.PixelWidth,
+                        (uint)mapBitmap.PixelHeight,
+                        DisplayInformation.GetForCurrentView().LogicalDpi,
+                        DisplayInformation.GetForCurrentView().LogicalDpi,
+                        pixelBuffer.ToArray());
+                    await encoder.FlushAsync();
+                }
+
+                HttpStringContent uidStringContent = new HttpStringContent(appSetting.Values["uid"].ToString());
+                HttpStringContent tokenStringContent = new HttpStringContent(appSetting.Values["token"].ToString());
+
+
+                // 构造需要上传的文件数据
+                IRandomAccessStreamWithContentType stream1 = await saveFile.OpenReadAsync();
+                HttpStreamContent streamContent = new HttpStreamContent(stream1);
+                HttpMultipartFormDataContent fileContent = new HttpMultipartFormDataContent();
+
+                fileContent.Add(streamContent, "photo", "蓝屏.png");
+                fileContent.Add(uidStringContent, "uid");
+                fileContent.Add(tokenStringContent, "token");
+
+                HttpResponseMessage response = await _httpClient.PostAsync(new Uri("http://106.184.7.12:8002/index.php/api/person/uploadimg"), fileContent).AsTask(_cts.Token);
+                head = Utils.ConvertUnicodeStringToChinese(await response.Content.ReadAsStringAsync().AsTask(_cts.Token));
+                Debug.WriteLine(head);
+            }
+            catch (Exception) { }
+            if (head != "")
+            {
+                JObject obj = JObject.Parse(head);
+                if (Int32.Parse(obj["status"].ToString()) != 200)
+                {
+                    Utils.Message(obj["info"].ToString());
+                    StatusProgressBar.Visibility = Visibility.Collapsed;
+                    StatusTextBlock.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    //Utils.Toast("发布成功");
+                    showStatus("保存成功", 1, Visibility.Collapsed);
+                    await Task.Delay(1500);
+                    Frame rootFrame = Window.Current.Content as Frame;
+                    rootFrame.GoBack();
+                }
+            }
+            else
+            {
+                StatusProgressBar.Visibility = Visibility.Collapsed;
+                StatusStackPanel.Background = new SolidColorBrush(Color.FromArgb(255, 50, 50, 50));
+                StatusTextBlock.Text = "网络未连接...";
+
+                await Task.Delay(2000);
+                StatusTextBlock.Visibility = Visibility.Collapsed;
             }
 
-            HttpStringContent uidStringContent = new HttpStringContent(appSetting.Values["uid"].ToString());
-            HttpStringContent tokenStringContent = new HttpStringContent(appSetting.Values["token"].ToString());
-
-
-            // 构造需要上传的文件数据
-            IRandomAccessStreamWithContentType stream1 = await saveFile.OpenReadAsync();
-            HttpStreamContent streamContent = new HttpStreamContent(stream1);
-            HttpMultipartFormDataContent fileContent = new HttpMultipartFormDataContent();
-
-            fileContent.Add(streamContent, "photo","蓝屏.png");
-            fileContent.Add(uidStringContent, "uid");
-            fileContent.Add(tokenStringContent, "token");
-
-            HttpResponseMessage response = await _httpClient.PostAsync(new Uri("http://106.184.7.12:8002/index.php/api/person/uploadimg"), fileContent).AsTask(_cts.Token);
-            string re =Utils.ConvertUnicodeStringToChinese( await response.Content.ReadAsStringAsync().AsTask(_cts.Token));
-            Debug.WriteLine(re);
-
-
-
-            if (Frame.CanGoBack)
-            {
-                Frame.GoBack();
-            }
         }
 
+        private async void showStatus(string text, int value, Windows.UI.Xaml.Visibility ProgressBarvisibility)
+        {
+            StatusStackPanel.Visibility = Visibility.Visible;
+            StatusProgressBar.Visibility = ProgressBarvisibility;
+            StatusTextBlock.Visibility = Visibility.Visible;
+            if (value == 1)
+                StatusStackPanel.Background = new SolidColorBrush(Color.FromArgb(255, 50, 50, 50));
+            else
+                StatusStackPanel.Background = null;
+            StatusTextBlock.Text = text;
 
+            if (ProgressBarvisibility == Visibility.Collapsed)
+            {
+                await Task.Delay(2000);
+                StatusTextBlock.Visibility = Visibility.Collapsed;
+                StatusStackPanel.Visibility = Visibility.Collapsed;
+                StatusProgressBar.Visibility = Visibility.Collapsed;
+                StatusStackPanel.Background = null;
+            }
+        }
 
 
     }

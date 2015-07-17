2a56e44699eeb7ee34166f1f06c781773ca7985e
yyx290799684 <yyx290799684@hotmail.com>
2015-07-17 14:36:03 +0800

    新建后台

---
+++ BackgroundTask/LetterBackgroundTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+
+namespace BackgroundTask
+{
+    public sealed class LetterBackgroundTask : IBackgroundTask
+    {
+        private ApplicationDataContainer appSetting;
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();  //获取 BackgroundTaskDeferral 对象，表示后台任务延期
+            //执行相关异步代码
+            deferral.Complete(); //所有的异步调用完成之后，释放延期，表示后台任务的完成
+        }
+
+    }
+}
---
+++ BackgroundTask/NetWork.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+using Windows.UI.Popups;
+
+namespace Date.Util
+{
+    class NetWork
+    {
+        public static async Task<string> getHttpWebRequest(string api, List<KeyValuePair<String, String>> paramList = null)
+        {
+            string content = "";
+            return await Task.Run(() =>
+           {
+               if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+               {
+                   try
+                   {
+                       HttpClient httpClient = new HttpClient();
+                       string uri = "http://106.184.7.12:8002/index.php/api" + api;
+                       HttpResponseMessage response = httpClient.PostAsync(new Uri(uri), new FormUrlEncodedContent(paramList)).Result;
+                       content = response.Content.ReadAsStringAsync().Result;
+                   }
+                   catch (Exception e)
+                   {
+                       Debug.WriteLine(e.Message + "网络请求异常");
+                   }
+               }
+               else
+               {
+               }
+               if (content.IndexOf("<!DOCTYPE") == 1)
+                   return "";
+               else
+                   return content;
+
+           });
+        }
+
+    }
+
+
+
+}

ea8b62f4d91c9316178e34b192c09ffb1b8ef2b1
Iamnvincible <ljlovemichael@hotmail.com>
2015-07-17 19:54:34 +0800

    状态

---
+++ Date/Data/DateStatusValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Date.Data
+{
+    class DateStatusValueConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string s = value as string;
+            if (s == "0")
+            {
+                return "已结束";
+            }
+            else if(s=="1")
+            {
+                return "受理中";
+            }
+            else if (s=="2")
+            {
+                return "成功";
+            }
+            else
+            {
+                return "未知";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
